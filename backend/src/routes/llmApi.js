f73906c0722ad384f7e63f078ce07c196e252ffd
Tyler Nguyen <tylernguyen518@gmail.com>
2025-08-13 11:11:15 -0700

    feat(llmTool): added tools for LLM

--- backend/src/routes/llmApi.js
+++ backend/src/routes/llmApi.js
@@ -1,5 +1,7 @@
 import { llmClass } from '../llm_handler/llmClass.js';
 import { llmTool, llmToolProperty } from '../llm_tools/toolClass.js';
+import { addItemTool, removeItemTool } from '../llm_tools/toolDefinitions.js';
+
 
 // Initialize LLM with a simple add tool
 function add(args) {
@@ -84,7 +86,7 @@ Do not provide choices for events, the player has the ability to do literally an
 
 
 const OPENAI_MODEL = process.env.OPENAI_MODEL || 'gpt-5-nano';
-const llm = new llmClass(OPENAI_MODEL, [addTool], system_prompt);
+const llm = new llmClass(OPENAI_MODEL, [addTool, addItemTool, removeItemTool], system_prompt);
 
 export const sendMessage = async (req, res) => {
     try {
