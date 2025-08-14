1d25bfd4a6e01475934925ae2a2f03469824eb03
Tyler Nguyen <tylernguyen518@gmail.com>
2025-08-13 17:23:22 -0700

    refactor(notes): comments

--- backend/models/gameState.js
+++ backend/models/gameState.js
@@ -1,3 +1,5 @@
+// here bc was using an increment to determine the number of moves for change the pricing
+// if progression is setup remove and use progression to change pricing as game goes on
 const gameState = {
     moves: 0,
 };
